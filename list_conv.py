2bc586f719b0c351957ea8b36bf1a580c3887411
rohit-chanaksh <93350371+rohit-chanaksh@users.noreply.github.com>
2024-09-14 00:46:36 +0530

    task 1 commit

---
+++ list_conv.py
@@ -0,0 +1,8 @@
+
+
+if __name__ == '__main__':
+    print('this is add py file')
+
+
+def list_converter(object):
+    return object.values()
---
+++ task_main.py
@@ -0,0 +1,19 @@
+
+
+from list_conv import list_converter
+
+
+
+if __name__ == '__main__':
+#Task 1
+    obj1 = {
+    'name': 'arc',
+    'age': 34
+    }
+
+    list = list(list_converter(obj1))
+    print(list)
+
+#Task 2
+    txt = 'awdu?23nb'
+
